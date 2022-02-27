a093cb70fe1e3ffda490d16cbb2be2a78bb3f9f7
pravinv1998 <pravin.wargad1998@gmail.com>
2022-02-28 00:00:57 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -15,14 +15,17 @@ function App() {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((res) => res.json())
       .then((data) => setTodos(data));
-  }, []);
+      console.log(todos)
+  }, [todos]);
+
+  
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/users/${user}`)
       .then((res) => res.json())
       .then((data) => setUserData(data));
   }, [user]);
-  // const [input, setInput] = useState("");
+  const [input, setInput] = useState("");
   // const handleInput = (value) => {
   //   setInput(value);
   // };
@@ -31,19 +34,25 @@ function App() {
     setUser(todo.userId);
     setTodoData(todo);
   };
+
+  const searchData = (todos) => {
+    // return todos 
+  }
   return (
-    <div className="flex flex-row   gap-4">
-      <div className="p-4  rounded-lg ">
+    <div className="flex flex-col sm:flex-row bg-slate-200  gap-4">
+      <div className="p-4   rounded-lg  ">
         <div className="flex  justify-between p-2 ">
           <h1 className="font-semibold sticky top-4 text-xl">Todos</h1>
           <input
             placeholder="Search"
-            className="p-3 text-lg border-b-2 sticky top-4  w-9/12"
+            value={input}
+            onChange={(e)=> setInput(e.target.value)}
+            className="p-3 text-lg border-b-2 sticky top-4 rounded-md  w-9/12"
             type="text"
           />
         </div>
-        <table className="w-full">
-          <thead className="bg-gray-50 border-b-2 sticky top-0 border-gray-200 text-left ">
+        <table className="w-full  ">
+          <thead className="bg-gray-50 rounded-md border-b-2 sticky top-0 border-gray-200 text-left ">
             <tr>
               <th className="p-3 text-md font-semibold text-left">ID</th>
               <th className="p-3 text-md font-semibold text-left">Title</th>
@@ -51,9 +60,9 @@ function App() {
               <th className="p-3 text-md font-semibold text-left">Action</th>
             </tr>
           </thead>
-          <tbody className="divide-y divide-gray-100">
-            {todos.map((todo) => (
-              <tr key={todo.id}>
+          <tbody className="divide-y  divide-gray-100">
+            {todos.filter(todo => todo.title.toLowerCase().includes(input.toLowerCase())).map((todo) => (
+              <tr className="bg-slate-50  " key={todo.id}>
                 <td className="p-3 text-sm text-gray-700 font-mono">
                   {todo.id}
                 </td>
@@ -82,13 +91,14 @@ function App() {
           </tbody>
         </table>
       </div>
-      <div className="p-4">
-        <div className=" sticky top-4">
-          <h1 className="">TodoID : {todoData.id} </h1>
-          <h1 className="capitalize">ToDo Title : {todoData.title} </h1>
-          <h1 className="">User ID : {todoData.userId} </h1>
-          <h1 className="">Name : {userData.name} </h1>
-          <h1 className="">Email: {userData.email}</h1>
+      <div className="p-4 ">
+        <div className="space-y-4 max-w-lg fixed  border-2 rounded-md p-8 bg-slate-50 sm:sticky top-4">
+          <h1 className="font-semibold text-xl " >User Data</h1>
+          <h1 className=" font-mono  ">TodoID : {todoData.id} </h1>
+          <h1 className=" font-mono capitalize">ToDo Title  :  {todoData.title} </h1>
+          <h1 className="font-mono">User ID  :  {todoData.userId} </h1>
+          <h1 className="font-mono">Name : {userData.name} </h1>
+          <h1 className="font-mono">Email : {userData.email}</h1>
         </div>
       </div>
     </div>
